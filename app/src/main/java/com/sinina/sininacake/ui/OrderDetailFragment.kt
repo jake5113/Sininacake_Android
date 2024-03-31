576a8e35e39304e01b7271e5d7e9fcce2685121e
jake <jongwon5113@gmail.com>
2024-03-31 15:17:33 +0900

    fix: 파일이름 변경

--- app/src/main/java/com/sinina/sininacake/ui/OrderDetailFragment.kt
+++ app/src/main/java/com/sinina/sininacake/ui/OrderDetailFragment.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sinina.sininacake.R
 
-class OderDetailFragment : AppCompatActivity() {
+class OrderDetailFragment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_oder_detail_fragment)
+        setContentView(R.layout.activity_order_detail_fragment)
     }
 }
